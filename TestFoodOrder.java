4ec8dcaf0250019c60950104f31b682da4a59924
Shivam Kumar <114653141+shivamkumar05@users.noreply.github.com>
2023-05-03 11:56:08 +0530

    Add files via upload

---
+++ TestFoodOrder.java
@@ -0,0 +1,12 @@
+public class TestFoodOrder {
+    public static void main(String[] args)
+    {
+        FoodOrder fo = new FoodOrder();
+
+        fo.displayMenu();
+
+        fo.order();
+        //lets compile and test
+    }
+    
+}
